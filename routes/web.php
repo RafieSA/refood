1b6e3d2b6bed4ef80ba1e7c90b4fd04a40343488
addinzulfikar <148080831+addinzulfikar@users.noreply.github.com>
2025-04-20 13:29:28 +0700

    still error kernel.php things

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RestaurantController; // ← tambahkan ini
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// route publik yang menampilkan data restoran
+Route::get('/restaurants', [RestaurantController::class, 'index']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
